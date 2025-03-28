67f367225d4ff623252efd725dd7c04f1bb2b19a
서정현 <150584831+CraftsManShip001@users.noreply.github.com>
2025-03-29 00:52:28 +0900

    CHORE :: 순서 수정

--- src/pages/docs/DocsPage/DocsPage.jsx
+++ src/pages/docs/DocsPage/DocsPage.jsx
@@ -58,22 +58,22 @@ export default function DocsPage(){
         {subName:'기능 설명'},
         {subName:'입출력 신호 연결'},
         {subName:'프로그램 실행'},
-        {subName:'원격'},
-        {subName:'단문 메시지 송출'},
-        {subName:'동영상 녹화'},
-        {subName:'배경 오디오 설정'},
-        {subName:'예약 송출'},
-        {subName:'외부장비 제어'},
-        {subName:'이미지,PPT 송출'},
         {subName:'입력 출력 설정'},
         {subName:'입출력 바이패스 설정'},
-        {subName:'AUDIO MIXER'},
+        {subName:'VIDEO MIXER'},
         {subName:'LIVE 입력 영상 송출'},
         {subName:'NET & USB'},
-        {subName:'기능 설명'},
+        {subName:'동영상 재생 송출'},
+        {subName:'이미지,PPT 송출'},
         {subName:'PC 화면 송출'},
+        {subName:'단문 메시지 송출'},
+        {subName:'AUDIO MIXER'},
+        {subName:'배경 오디오 설정'},
         {subName:'Play List 순서 송출'},
-        {subName:'VIDEO MIXER'}
+        {subName:'예약 송출'},
+        {subName:'동영상 녹화'},
+        {subName:'외부장비 제어'},
+        {subName:'원격'}
     ]
       
     
